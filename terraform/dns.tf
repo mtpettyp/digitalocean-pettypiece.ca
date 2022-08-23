674adc78d960c35dea51baacec71b7c7a7cd9cf8
Mike Pettypiece <mike@pettypiece.ca>
2022-08-23 11:18:40 -0400

    Add uptime.pettypiece.ca

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -33,3 +33,11 @@ resource "digitalocean_record" "wireguard_pettypiece_ca" {
   value  = digitalocean_droplet.pettypiece_ca.ipv4_address
   ttl    = 60
 }
+
+resource "digitalocean_record" "uptime_pettypiece_ca" {
+  domain = data.digitalocean_domain.pettypiece_ca.name
+  type   = "A"
+  name   = "uptime"
+  value  = digitalocean_droplet.pettypiece_ca.ipv4_address
+  ttl    = 60
+}
